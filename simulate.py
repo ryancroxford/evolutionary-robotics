f74bfa53bd36c28fa14b26510a3eea1517e94a37
Ryan Croxford <ryan.croxford@uvm.edu>
2021-02-15 11:55:16 -0500

    Generate a 6x6x10 output of links

--- simulate.py
+++ simulate.py
@@ -1,9 +1,14 @@
 import pybullet as p
+import pybullet_data
 import time
 
 physicsClient = p.connect(p.GUI)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
 
-p.loadSDF("box.sdf")
+p.setGravity(0, 0, -9.8)
+planeId = p.loadURDF("plane.urdf")
+
+p.loadSDF("boxes.sdf")
 
 for i in range(1000):
     p.stepSimulation()
