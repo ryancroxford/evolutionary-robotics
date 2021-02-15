f026e2acbe9e65016f57f3fa1ae6a5761d63ad5c
Ryan Croxford <ryan.croxford@uvm.edu>
2021-02-15 14:49:19 -0500

    create 2 legged robot

--- generate.py
+++ generate.py
@@ -1,33 +1,46 @@
 import pyrosim.pyrosim as pyrosim
 
-pyrosim.Start_SDF("boxes.sdf")
 
-# initalize size and positions
-length = 1
-width = 1
-height = 1
-x = 0
-y = 0
-z = height/2
+def Create_World():
+    pyrosim.Start_SDF("world.sdf")
+    length = 1
+    width = 1
+    height = 1
+    x = 2
+    y = 2
+    z = height/2
+    pyrosim.Send_Cube(name="Box", pos=[x+2, y+2, z], size=[length, width, height])
 
-# loop to create a 6x6 spread of 10 link tall towers
-for i in range(6):
-    for j in range(6):
-        # reset size and initial z position
-        length = 1
-        width = 1
-        height = 1
-        z = height/2
-        for k in range(10):
-            x = i
-            y = j
-            pyrosim.Send_Cube(name="Box", pos=[x, y, z], size=[length, width, height])
-            # stack the next link on top of the previous
-            z += height
-            # make the next link's dimensions 90% as big
-            height *= 0.9
-            width *= 0.9
-            length *= 0.9
+    pyrosim.End()
 
 
-pyrosim.End()
+def Create_Robot():
+    pyrosim.Start_URDF("body.urdf")
+    length = 1
+    width = 1
+    height = 1
+    x = 1.5
+    y = 0
+    z = 1.5
+    pyrosim.Send_Cube(name="Torso", pos=[x, y, z], size=[length, width, height])
+    # Create back leg
+    pyrosim.Send_Joint(name="Torso_Back_Leg", parent="Torso", child="Back_Leg",
+                       type="revolute", position="1.0 0.0 1.0")
+    x = -0.5
+    z = -0.5
+    pyrosim.Send_Cube(name="Back_Leg", pos=[x, y, z], size=[length, width, height])
+    # Create front leg
+    pyrosim.Send_Joint(name="Torso_Front_Leg", parent="Torso", child="Front_Leg",
+                       type="revolute", position="2.0 0.0 1.0")
+    x = 0.5
+    z = -0.5
+    pyrosim.Send_Cube(name="Front_Leg", pos=[x, y, z], size=[length, width, height])
+    pyrosim.End()
+
+
+def main():
+    Create_World()
+    Create_Robot()
+
+
+main()
