9d80c7b0c12183870104244a9c5e8a3b9ac7949f
Ryan Croxford <ryan.croxford@uvm.edu>
2021-02-28 15:43:26 -0500

    finish G. motors

--- analyze.py
+++ analyze.py
@@ -3,9 +3,18 @@ import matplotlib.pyplot as plt
 
 backLegSensorValues = np.load("./data/back_leg_touch.npy")
 frontLegSensorValues = np.load("./data/front_leg_touch.npy")
+targetAnglesBackLeg = np.load("./data/target_angles_Back_Leg.npy")
+targetAnglesFrontLeg = np.load("./data/target_angles_Front_Leg.npy")
 
-plt.plot(backLegSensorValues, label="Back Leg", linewidth=4)
-plt.plot(frontLegSensorValues, label="Front Leg")
+# plt.plot(backLegSensorValues, label="Back Leg", linewidth=4)
+# plt.plot(frontLegSensorValues, label="Front Leg")
+
+
+plt.plot(targetAnglesBackLeg, label="Back Leg Motor", linewidth = 4)
+plt.plot(targetAnglesFrontLeg, label="Front Leg Motor")
+plt.ylabel('sin(x)')
+plt.axis('tight')
+
 plt.legend()
 
 plt.show()
