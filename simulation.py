e2b139e20b005a57ea423cf02476b4afed9a2606
Ryan Croxford <ryan.croxford@uvm.edu>
2021-03-08 22:04:19 -0500

    finished refactoring

--- simulation.py
+++ simulation.py
@@ -3,6 +3,7 @@ from robot import Robot
 import pybullet as p
 import pybullet_data
 import constants as c
+import time
 
 
 class Simulation:
@@ -12,3 +13,13 @@ class Simulation:
         p.setGravity(c.ZERO, c.ZERO, c.GRAVITY)
         self.world = World()
         self.robot = Robot()
+
+    def Run(self):
+        for t in range(c.ITERATIONS):
+            p.stepSimulation()
+            self.robot.Sense(t)
+            self.robot.Act(t)
+            time.sleep(c.TIME_STEP)
+
+    def __del__(self):
+        p.disconnect()
